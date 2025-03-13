38d86bb03cca8ac85b91995cbc74762425d09417
Ourai Lin <ourairyu@gmail.com>
2025-03-13 18:42:06 +0800

    feat(site): add Hexo site files

---
+++ .knosys/scripts/command/gen.js
@@ -0,0 +1,30 @@
+const { resolve: resolvePath } = require('path');
+const { existsSync } = require('fs');
+
+const { resolveRootPath, scanAndSortByAsc, ensureDirExists, getLocalDataRoot, getLocalDocRoot } = require('../helper');
+const { createWeeklyGenerator, createNoteGenerator, createProjectGenerator, createRepoGenerator, generateBookmarks } = require('../generator');
+
+module.exports = {
+  execute: dataSource => {
+    const srcPath = resolvePath(resolveRootPath(), dataSource);
+
+    if (!existsSync(srcPath)) {
+      return;
+    }
+
+    [getLocalDataRoot(), getLocalDocRoot()].forEach(distPath => ensureDirExists(distPath, true));
+
+    const sourceRootPath = resolvePath(srcPath, 'data');
+    const sharedRootPath = resolvePath(srcPath, 'shared');
+
+    const generators = {
+      weeklies: createWeeklyGenerator(sourceRootPath, sharedRootPath),
+      notes: createNoteGenerator(sourceRootPath, sharedRootPath),
+      projects: createProjectGenerator(sourceRootPath, sharedRootPath),
+      repos: createRepoGenerator(sourceRootPath, sharedRootPath),
+    };
+
+    scanAndSortByAsc(sharedRootPath).forEach(collection => generators[collection] && generators[collection]());
+    generateBookmarks(sourceRootPath);
+  },
+};
